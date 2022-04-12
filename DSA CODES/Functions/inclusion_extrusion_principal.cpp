0e37c9f073e99aa34de23af9ed0e6cec5839e885
Vishnu Khokhar <72308651+vishnukhokhar@users.noreply.github.com>
2022-04-12 14:00:05 +0530

    Add files via upload

---
+++ DSA CODES/Functions/inclusion_extrusion_principal.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int divisible(int n, int a, int b)
+{
+    int c1=n/a;
+    int c2=n/b;
+
+    int c3=n/(a*b);
+
+    return (c1+c2-c3);
+}
+
+int main()
+{
+    int n,a,b;
+
+    cout<<"Enter the number to check ";
+    cin>>n;
+    cout<<"Enter the other numbers ";
+    cin>>a>>b;
+    cout<<divisible(n,a,b);
+}
