94b499d38fb67e36b3b284115ce204f0b1de8374
Vishnu Khokhar <72308651+vishnukhokhar@users.noreply.github.com>
2022-04-25 23:00:18 +0530

    Add files via upload

---
+++ DSA CODES/Heap/decrease_delete_key.cpp
@@ -0,0 +1,121 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class minheap
+{
+    int *arr;
+    int size;
+    int cap;
+
+    public:
+    minheap(int c)
+    {
+        cap=c;
+        size=0;
+        arr=new int(c);
+    }
+
+    int left(int i)
+    {
+        return (2*i +1);
+    }
+    int right(int i)
+    {
+        return(2*i +2);
+    }
+
+    int parent(int i)
+    {
+        return(i-1)/2;
+    }
+
+    void insert(int x)
+    {
+        if(size==cap)
+        {
+            return;
+        }
+
+        size++;
+        arr[size-1]=x;
+
+        for(int i=size-1; i!=0 && arr[parent(i)]>arr[i];)
+        {
+            swap(arr[i],arr[parent(i)]);
+            i=parent(i);
+        }
+        
+    }
+
+    void minheapify(int i)
+    {
+        int lt=left(i);
+        int rt=right(i);
+        int smallest=i;
+
+        if(lt<size && arr[lt]<arr[i])
+        {
+            smallest=i;
+        }
+        if(rt<size && arr[rt]<arr[smallest])
+        {
+            smallest=rt;
+        }
+
+        if(smallest!=i)
+        {
+            swap(arr[i],arr[smallest]);
+            minheapify(smallest);
+        }
+    }
+
+    int extract_min()
+    {
+        if(size<=0)
+        {
+            return INT_MAX;
+        }
+
+        if(size==1)
+        {
+            size--;
+            return arr[0];
+
+        }
+
+        swap(arr[0],arr[size-1]);
+        size--;
+        minheapify(0);
+        return arr[size];
+    }
+
+    void decrease_key(int i, int x)
+    {
+        arr[i]=x;
+        while( i!=0 && arr[i]<arr[parent(i)])
+        {
+            swap(arr[i],arr[parent(i)]);
+            i=parent(i);
+        }
+    }
+
+    void delete_key(int i)
+    {
+        decrease_key(i, INT_MIN);
+        extract_min();
+    }
+};
+
+int main() 
+{ 
+    minheap h(11);
+    h.insert(3); 
+    h.insert(2);
+    h.delete_key(0);
+    h.insert(15);
+    h.insert(20);
+    cout << h.extract_min() << endl;
+    h.decrease_key(2, 1);
+    cout << h.extract_min() << endl;
+    return 0;
+} 
